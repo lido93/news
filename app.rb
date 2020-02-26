0e7efc6ade539561d9f65d98487ad9702fda891a
lido93 <lisa.dong@kellogg.northwestern.edu>
2020-02-26 02:23:11 +0000

    update 4

--- app.rb
+++ app.rb
@@ -29,19 +29,19 @@ get "/news" do
 
     @conditions = @forecast["currently"]["summary"]
 
-#get future forecast 
-# temp_array = []
-# condition_array = []
-# low_temp_array = []
-# day_of_week = [0,1,2,3,4,5,6]
-#     for day in @forecast["daily"]["data"]
-#     temp_array << "#{day["temperatureHigh"]}" 
-#     condition_array << "#{day["summary"]}" 
-#     low_temp_array << "#{day["temperatureLow"]}" 
-#     end
-# @future_temp = temp_array[0..6]
-# @future_condition = condition_array[0..6]
-# @future_low_temp = low_temp_array[0..6]
+# get future forecast 
+temp_array = []
+condition_array = []
+low_temp_array = []
+day_of_week = [0,1,2,3,4,5,6]
+    for day in @forecast["daily"]["data"]
+    temp_array << "#{day["temperatureHigh"]}" 
+    condition_array << "#{day["summary"]}" 
+    low_temp_array << "#{day["temperatureLow"]}" 
+    end
+@future_temp = temp_array[0..6]
+@future_condition = condition_array[0..6]
+@future_low_temp = low_temp_array[0..6]
 
 
 @url = "https://newsapi.org/v2/top-headlines?country=us&apiKey=9626f0bdeeac4f4a8999fe2a436b864c"
